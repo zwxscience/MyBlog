ab1d49253bdd6d8b50aa7c1729033ace7692b73f
xavier <zwxscience@gmail.com>
2017-08-23 23:04:19 +0800

    Update updateResult.js

--- js/updateResult.js
+++ js/updateResult.js
@@ -4,7 +4,7 @@ setTimeout(function() {
 	mainPath = mainPath == "/"? window.location.href:mainPath;//frontPage
 	var title = ($(document).attr("title") == null) ? $('title').text() : $(document).attr("title");
 		 $.ajax({
-			url: 'http://www.zhangweixiang.com/visitinfo.ashx?url='+mainPath+'&title=' + $.trim(title),
+			url: 'http://www.zhangweixiang.com/visitinfo.ashx?url='+mainPath+'&from=blog&title=' + $.trim(title),
 			dataType: 'jsonp',
 			timeout: 1000 * 3, // 3 sec
 			jsonp: "callback",  
