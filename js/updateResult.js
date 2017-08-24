b17d4088f999a94af86ab656af84e2f7cb0cb252
xavier <zwxscience@gmail.com>
2017-08-24 14:51:30 +0800

    Update updateResult.js

--- js/updateResult.js
+++ js/updateResult.js
@@ -1,45 +1,9 @@
 
-LazyLoad.js('http://zhangweixiang.com/js/jquery.min.js', function () {
 var queryId = window.location.pathname.indexOf('?');
 var mainPath = queryId >= 0 ? window.location.pathname.slice(0, queryId) : window.location.pathname;
 mainPath = mainPath == "/"? window.location.href:mainPath;//frontPage
 var title = ($(document).attr("title") == null) ? $('title').text() : $(document).attr("title");
-function dealvisitCount()
-{
-		 $.ajax({
-			url: 'http://www.zhangweixiang.com/visitInfo.ashx?method=get&from=blog',
-			dataType: 'jsonp',
-			timeout: 1000 * 3, // 3 sec
-			jsonp: "callback",  
-				jsonpCallback: "jsonpCallback",
-			success: function(data) {
-				processSinglePage(data);
-			},
-			error: function(xhr,status,error) {
-				// if fail to get up-to-date data from mysite, get cached local version
-				console.log(xhr);
-				console.log('Failed to get page view from my site!');	
-			}
-		});	
-	setTimeout("dealvisitCount()",3000);
-}
 
-var pagecnt = 0;
-function processSinglePage(rows) {
-	if (rows == undefined) {
-		return;
-	}
-	var myPath = $.trim(title);
-
-	var currentCount = getVisitCount(rows,myPath);
-	if(pagecnt< currentCount)
-	{
-		pagecnt = currentCount;
-		$("#pageview").html("访问量 <span id = 'visitcount' >"+ currentCount+"</span>").show();
-		$('#visitcount').addClass('visitcount');
-		setTimeout("$('#visitcount').removeClass('visitcount')",1500);
-	}
-}
 setTimeout(function() {
 
 		 $.ajax({
@@ -56,8 +20,6 @@ setTimeout(function() {
 				console.log('Failed to get page view from my site!');	
 			}
 		});	
-		
-		dealvisitCount();
         }, 1000);
-});
+
 
