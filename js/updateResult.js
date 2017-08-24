8fe281884eab8f54248bb59e4f53f68c7b64e42c
xavier <zwxscience@gmail.com>
2017-08-24 14:07:43 +0800

    Update updateResult.js

--- js/updateResult.js
+++ js/updateResult.js
@@ -17,4 +17,39 @@ setTimeout(function() {
 				console.log('Failed to get page view from my site!');	
 			}
 		});	
-        }, 2000);
+setInterval ("getvisitCount()", 3000);
+function getvisitCount()
+{
+		 $.ajax({
+			url: 'http://www.zhangweixiang.com/visitInfo.ashx?method=get&from=blog',
+			dataType: 'jsonp',
+			timeout: 1000 * 3, // 3 sec
+			jsonp: "callback",  
+    			jsonpCallback: "jsonpCallback",
+			success: function(data) {
+				processSinglePage(data);
+			},
+			error: function(xhr,status,error) {
+				// if fail to get up-to-date data from mysite, get cached local version
+				console.log(xhr);
+				console.log('Failed to get page view from my site!');	
+			}
+		});	
+}
+var cnt = 0;
+function processSinglePage(rows) {
+    if (rows == undefined) {
+        return;
+    }
+    var myPath = $.trim(title);
+
+	var currentCount = getVisitCount(rows,myPath);
+	if(cnt < currentCount)
+	{
+		cnt = currentCount;
+		$("#pageview").html("访问量 <span id = 'visitcount' >"+ currentCount+"</span>").show();
+		$('#visitcount').addClass('visitcount');
+		setTimeout("$('#visitcount').removeClass('visitcount')",1500);
+	}
+}
+        }, 1000);
